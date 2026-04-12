fceb23b733483fe160a6c19f2de815a512545fb1
adeogu <Adeogu1234@gmail.com>
2026-04-12 23:31:58 +0100

    Update seed data for Actor Service

--- actor-service/src/main/resources/data.sql
+++ actor-service/src/main/resources/data.sql
@@ -1,5 +1,4 @@
--- Seed data for Actor Service (H2 local dev only)
--- movieId values correspond to the seeded movies in the Movie service
+
 INSERT INTO actors (name, nationality, movie_id) VALUES ('Marlon Brando', 'American', 1);
 INSERT INTO actors (name, nationality, movie_id) VALUES ('Leonardo DiCaprio', 'American', 2);
 INSERT INTO actors (name, nationality, movie_id) VALUES ('John Travolta', 'American', 3);
