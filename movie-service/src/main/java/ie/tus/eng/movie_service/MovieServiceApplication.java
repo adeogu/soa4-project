9510220ee2bdd7986a4a3de6c1b973464aa47dfa
adeogu <Adeogu1234@gmail.com>
2026-04-12 23:34:04 +0100

    Clean up comments in MovieServiceApplication
    Removed unnecessary comments from MovieServiceApplication.

--- movie-service/src/main/java/ie/tus/eng/movie_service/MovieServiceApplication.java
+++ movie-service/src/main/java/ie/tus/eng/movie_service/MovieServiceApplication.java
@@ -3,15 +3,7 @@ package ie.tus.eng.movie_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * MovieServiceApplication - Service B (Producer)
- *
- * This is the Movie microservice. It acts as the "producer" in the
- * Request-Response communication pattern. The Actor service (Service A)
- * will call this service to retrieve movie details.
- *
- * Runs on port 8081 (see application.properties).
- */
+
 @SpringBootApplication
 public class MovieServiceApplication {
     public static void main(String[] args) {
